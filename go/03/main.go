72b0d4076e8d367dc1d46f34810057a036b24db8
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-06-18 16:52:59 +0900

    added 003

---
+++ go/03/main.go
@@ -0,0 +1,22 @@
+// 03. 円周率
+// "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+func main() {
+	s := "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+	tokens := strings.Split(strings.Replace(s, ",", "", -1), " ")
+
+	counts := make([]int, 0, len(tokens))
+	for _, token := range tokens {
+		counts = append(counts, utf8.RuneCountInString(token))
+	}
+
+	fmt.Println(counts)
+}
