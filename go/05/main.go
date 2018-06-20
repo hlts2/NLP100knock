fbc0014f8685b9d90a4d45d9c0cfb592381ef736
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-06-20 15:45:54 +0900

    added 05

---
+++ go/05/main.go
@@ -0,0 +1,57 @@
+// 05. n-gram
+// 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func wordNgram(tokens []string, n int) [][]string {
+	ss := make([][]string, 0, len(tokens)-(n-1))
+
+	for i := range tokens {
+		if len(tokens) > i+n {
+			ss = append(ss, tokens[i:i+n])
+		} else {
+			ss = append(ss, tokens[i:])
+			break
+		}
+	}
+
+	return ss
+}
+
+func runeSlice2StringSlice(runes []rune) []string {
+	ss := make([]string, 0, len(runes))
+
+	for _, r := range runes {
+		ss = append(ss, string(r))
+	}
+	return ss
+}
+
+func charNgram(s string, n int) [][]string {
+	runes := []rune(s)
+
+	ss := make([][]string, 0, len(runes)-(n-1))
+	for i := range runes {
+		if len(runes) > i+n {
+			ss = append(ss, runeSlice2StringSlice(runes[i:i+n]))
+		} else {
+			ss = append(ss, runeSlice2StringSlice(runes[i:]))
+			break
+		}
+	}
+
+	return ss
+}
+
+func main() {
+	s := "I am an NLPer"
+
+	tokens := strings.Split(s, " ")
+	fmt.Println(wordNgram(tokens, 2))
+	fmt.Println(charNgram(s, 2))
+}
